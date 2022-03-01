0cdd3e31ed778c44d36e636ae3d76b1a4e2f8e80
Denny Prastyo <dennyprastyo9f@gmail.com>
2022-03-01 23:28:15 +0700

    initial commit

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use App\Cart;
+use App\Transaction;
+
+
+class CheckoutController extends Controller
+{
+
+    public function __construct(){
+    $this->middleware('auth');
+    }
+
+    public function store(){
+        $carts = Cart::where('user_id', Auth::user()->id);
+        $cartUser = $carts->get();
+
+        $transaction = Transaction::create([
+            'user_id' => Auth::user()->id,
+        ]);
+
+        // relationship insert : ambil function yang ada dalam model
+        foreach ($cartUser as $cart) {
+            $transaction->detail()->create([
+                'product_id' => $cart->product_id,
+                'qty' => $cart->qty
+            ]);
+        }
+
+        $carts->delete();
+        return redirect('/');
+    }
+}
